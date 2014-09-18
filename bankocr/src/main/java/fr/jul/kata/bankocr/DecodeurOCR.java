997424788c2e06dc6f5e52694586f9dd385cd2f9
JDURAND <julien19durand@hotmail.fr>
2014-09-18 20:58:41 +0200

    premier test 000000000

--- bankocr/src/main/java/fr/jul/kata/bankocr/DecodeurOCR.java
+++ bankocr/src/main/java/fr/jul/kata/bankocr/DecodeurOCR.java
@@ -2,4 +2,14 @@ package fr.jul.kata.bankocr;
 
 public class DecodeurOCR {
 
+	private String entree;
+
+	public DecodeurOCR(String entree) {
+		entree = this.entree;
+	}
+
+	public String decoder() {
+		return "000000000";
+	}
+
 }
--- bankocr/src/test/java/fr/jul/kata/bankocr/DecodeurOCRTest.java
+++ bankocr/src/test/java/fr/jul/kata/bankocr/DecodeurOCRTest.java
@@ -1,5 +1,23 @@
 package fr.jul.kata.bankocr;
 
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.junit.Test;
+
 public class DecodeurOCRTest {
 
+	@Test
+	public void peutReconnaitre000000000() {
+		DecodeurOCR decodeurOCR = new DecodeurOCR(fabriquerUneEntreeDeZero());
+
+		String entréeDecodé = decodeurOCR.decoder();
+
+		assertThat(entréeDecodé).isEqualTo("000000000");
+
+	}
+
+	private String fabriquerUneEntreeDeZero() {
+		return " _  _  _  _  _  _  _  _  _ " + "| || || || || || || || || |" + "|_||_||_||_||_||_||_||_||_|"
+				+ "                           ";
+	}
 }
