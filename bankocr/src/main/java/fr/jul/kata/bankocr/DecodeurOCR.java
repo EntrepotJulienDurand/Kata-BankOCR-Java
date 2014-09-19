1303066f386dbc74c57d4788936a6bf2f20a245f
Julien Durand <julien.durand01-ext@pole-emploi.fr>
2014-09-19 17:10:35 +0200

    début d'implémentation

--- bankocr/src/main/java/fr/jul/kata/bankocr/DecodeurOCR.java
+++ bankocr/src/main/java/fr/jul/kata/bankocr/DecodeurOCR.java
@@ -1,15 +1,36 @@
 package fr.jul.kata.bankocr;
 
 public class DecodeurOCR {
-
-	private String entree;
-
-	public DecodeurOCR(String entree) {
-		entree = this.entree;
-	}
-
-	public String decoder() {
-		return "000000000";
-	}
-
+    
+    private static final String MOTIF_CHIFFRE_ZERO = " _ " + "| |" + "|_|";
+    
+    public String decoder(String entree) {
+        String resultat = "";
+        
+        for (int i = 0; i < (entree.length()); i++) {
+            int index = i;
+            String ligneCaractere1;
+            if (i == 0) {
+                index = 1;
+                ligneCaractere1 = entree.substring(i, i + 3);
+            } else {
+                ligneCaractere1 = entree.substring(i * 3, (i * 3) + 3);
+            }
+            String ligneCaractere2 = entree.substring(index * 9 * 3, (index * 9 * 3) + 3);
+            String ligneCaractere3 = entree.substring(index * 18 * 3, (index * 18 * 3) + 3);
+            
+            String chiffre = ligneCaractere1 + ligneCaractere2 + ligneCaractere3;
+            
+            if (MOTIF_CHIFFRE_ZERO.equals(chiffre)) {
+                resultat += "0";
+            } else {
+                resultat += "1";
+            }
+            System.out.println(ligneCaractere1);
+            System.out.println(ligneCaractere2);
+            System.out.println(ligneCaractere3);
+        }
+        
+        return resultat;
+    }
 }
--- bankocr/src/test/java/fr/jul/kata/bankocr/DecodeurOCRTest.java
+++ bankocr/src/test/java/fr/jul/kata/bankocr/DecodeurOCRTest.java
@@ -8,15 +8,27 @@ public class DecodeurOCRTest {
     
     @Test
     public void peutReconnaitre000000000() {
-        DecodeurOCR decodeurOCR = new DecodeurOCR(this.fabriquerUneEntreeDeZero());
+        DecodeurOCR decodeurOCR = new DecodeurOCR();
         
-        String entreeDecode = decodeurOCR.decoder();
+        String entreeDecodee = decodeurOCR.decoder(this.fabriquerUneEntreeDeZero());
         
-        assertThat(entreeDecode).isEqualTo("000000000");
-        
+        assertThat(entreeDecodee).isEqualTo("000000000");
     }
     
     private String fabriquerUneEntreeDeZero() {
         return " _  _  _  _  _  _  _  _  _ " + "| || || || || || || || || |" + "|_||_||_||_||_||_||_||_||_|" + "                           ";
+    }
+    
+    @Test
+    public void peutReconnaitre111111111() {
+        DecodeurOCR decodeurOCR = new DecodeurOCR();
+        
+        String entreedecodee = decodeurOCR.decoder(this.fabriquerUneEntreeDeUn());
+        
+        assertThat(entreedecodee).isEqualTo("111111111");
+    }
+    
+    private String fabriquerUneEntreeDeUn() {
+        return "                           " + "  |  |  |  |  |  |  |  |  |" + "  |  |  |  |  |  |  |  |  |" + "                           ";
     }
 }
