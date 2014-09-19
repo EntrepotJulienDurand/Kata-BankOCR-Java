90f6410bb109d1daab40a70c5c6628756c0e6c4d
Julien Durand <julien.durand01-ext@pole-emploi.fr>
2014-09-19 12:05:37 +0200

    suppression des accents

--- bankocr/src/test/java/fr/jul/kata/bankocr/DecodeurOCRTest.java
+++ bankocr/src/test/java/fr/jul/kata/bankocr/DecodeurOCRTest.java
@@ -5,19 +5,18 @@ import static org.fest.assertions.Assertions.assertThat;
 import org.junit.Test;
 
 public class DecodeurOCRTest {
-
-	@Test
-	public void peutReconnaitre000000000() {
-		DecodeurOCR decodeurOCR = new DecodeurOCR(fabriquerUneEntreeDeZero());
-
-		String entréeDecodé = decodeurOCR.decoder();
-
-		assertThat(entréeDecodé).isEqualTo("000000000");
-
-	}
-
-	private String fabriquerUneEntreeDeZero() {
-		return " _  _  _  _  _  _  _  _  _ " + "| || || || || || || || || |" + "|_||_||_||_||_||_||_||_||_|"
-				+ "                           ";
-	}
+    
+    @Test
+    public void peutReconnaitre000000000() {
+        DecodeurOCR decodeurOCR = new DecodeurOCR(this.fabriquerUneEntreeDeZero());
+        
+        String entreeDecode = decodeurOCR.decoder();
+        
+        assertThat(entreeDecode).isEqualTo("000000000");
+        
+    }
+    
+    private String fabriquerUneEntreeDeZero() {
+        return " _  _  _  _  _  _  _  _  _ " + "| || || || || || || || || |" + "|_||_||_||_||_||_||_||_||_|" + "                           ";
+    }
 }
