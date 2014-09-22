70b1f0b0d5b563b41767dcde3659cfef007e947c
Julien Durand <julien.durand01-ext@pole-emploi.fr>
2014-09-22 17:00:40 +0200

    première ipmplémentation des 0 et 1

--- bankocr/src/main/java/fr/jul/kata/bankocr/DecodeurOCR.java
+++ bankocr/src/main/java/fr/jul/kata/bankocr/DecodeurOCR.java
@@ -2,22 +2,45 @@ package fr.jul.kata.bankocr;
 
 public class DecodeurOCR {
     
+    private static final int DEBUT_LIGNE_3 = 54;
+    private static final int DEBUT_LIGNE_2 = 27;
+    private static final int TAILLE_DU_CHIFFRE = 3;
     private static final String MOTIF_CHIFFRE_ZERO = " _ " + "| |" + "|_|";
     
     public String decoder(String entree) {
         String resultat = "";
+        // String ligneCaractere1 = entree.substring(0, 3);
+        // String ligneCaractere2 = entree.substring(27, 30);
+        // String ligneCaractere3 = entree.substring(54, 57);
+        //
+        // String chiffre = ligneCaractere1 + ligneCaractere2 + ligneCaractere3;
+        //
+        // System.out.println(ligneCaractere1);
+        // System.out.println(ligneCaractere2);
+        // System.out.println(ligneCaractere3);
+        //
+        // ligneCaractere1 = entree.substring(3, 6);
+        // ligneCaractere2 = entree.substring(30, 33);
+        // ligneCaractere3 = entree.substring(60, 63);
+        //
+        // System.out.println(ligneCaractere1);
+        // System.out.println(ligneCaractere2);
+        // System.out.println(ligneCaractere3);
+        //
+        // ligneCaractere1 = entree.substring(6, 9);
+        // ligneCaractere2 = entree.substring(33, 36);
+        // ligneCaractere3 = entree.substring(63, 66);
+        //
+        // System.out.println(ligneCaractere1);
+        // System.out.println(ligneCaractere2);
+        // System.out.println(ligneCaractere3);
         
-        for (int i = 0; i < (entree.length()); i++) {
-            int index = i;
-            String ligneCaractere1;
-            if (i == 0) {
-                index = 1;
-                ligneCaractere1 = entree.substring(i, i + 3);
-            } else {
-                ligneCaractere1 = entree.substring(i * 3, (i * 3) + 3);
-            }
-            String ligneCaractere2 = entree.substring(index * 9 * 3, (index * 9 * 3) + 3);
-            String ligneCaractere3 = entree.substring(index * 18 * 3, (index * 18 * 3) + 3);
+        for (int i = 0; i < 9; i++) {
+            int indexDebutChiffre = i * TAILLE_DU_CHIFFRE;
+            int indexFinChiffre = indexDebutChiffre + TAILLE_DU_CHIFFRE;
+            String ligneCaractere1 = entree.substring(indexDebutChiffre, indexFinChiffre);
+            String ligneCaractere2 = entree.substring(indexDebutChiffre + DEBUT_LIGNE_2, indexFinChiffre + DEBUT_LIGNE_2);
+            String ligneCaractere3 = entree.substring(indexDebutChiffre + DEBUT_LIGNE_3, indexFinChiffre + DEBUT_LIGNE_3);
             
             String chiffre = ligneCaractere1 + ligneCaractere2 + ligneCaractere3;
             
