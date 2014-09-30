e876a9bd34b06e8ab97e6abc0d2cb7e415b91320
JDURAND <julien19durand@hotmail.fr>
2014-09-30 22:27:23 +0200

    User story 2 - Cheksum

--- bankocr/src/main/java/fr/jul/kata/bankocr/DecodeurOCR.java
+++ bankocr/src/main/java/fr/jul/kata/bankocr/DecodeurOCR.java
@@ -4,65 +4,72 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class DecodeurOCR {
-    
-    private static final int DEBUT_LIGNE_3 = 54;
-    private static final int DEBUT_LIGNE_2 = 27;
-    private static final int TAILLE_DU_CHIFFRE = 3;
-    private static final String MOTIF_CHIFFRE_ZERO = " _ " + "| |" + "|_|";
-    private static final String MOTIF_CHIFFRE_UN = "   " + "  |" + "  |";
-    private static final String MOTIF_CHIFFRE_DEUX = " _ " + " _|" + "|_ ";
-    private static final String MOTIF_CHIFFRE_TROIS = " _ " + " _|" + " _|";
-    private static final String MOTIF_CHIFFRE_QUATRE = "   " + "|_|" + "  |";
-    private static final String MOTIF_CHIFFRE_CINQ = " _ " + "|_ " + " _|";
-    private static final String MOTIF_CHIFFRE_SIX = " _ " + "|_ " + "|_|";
-    private static final String MOTIF_CHIFFRE_SEPT = " _ " + "  |" + "  |";
-    private static final String MOTIF_CHIFFRE_HUIT = " _ " + "|_|" + "|_|";
-    private static final String MOTIF_CHIFFRE_NEUF = " _ " + "|_|" + " _|";
-    private static Map<String, String> correspondanceMotifChiffre = new HashMap<String, String>();
-    static {
-        correspondanceMotifChiffre.put(MOTIF_CHIFFRE_ZERO, "0");
-        correspondanceMotifChiffre.put(MOTIF_CHIFFRE_UN, "1");
-        correspondanceMotifChiffre.put(MOTIF_CHIFFRE_DEUX, "2");
-        correspondanceMotifChiffre.put(MOTIF_CHIFFRE_TROIS, "3");
-        correspondanceMotifChiffre.put(MOTIF_CHIFFRE_QUATRE, "4");
-        correspondanceMotifChiffre.put(MOTIF_CHIFFRE_CINQ, "5");
-        correspondanceMotifChiffre.put(MOTIF_CHIFFRE_SIX, "6");
-        correspondanceMotifChiffre.put(MOTIF_CHIFFRE_SEPT, "7");
-        correspondanceMotifChiffre.put(MOTIF_CHIFFRE_HUIT, "8");
-        correspondanceMotifChiffre.put(MOTIF_CHIFFRE_NEUF, "9");
-    }
-    
-    public String decoder(String entree) {
-        String chiffre = "";
-        
-        for (int i = 0; i < 9; i++) {
-            String chiffreOCR = this.decouperLeIemeChiffre(entree, i);
-            
-            chiffre += this.determinerChiffreSelonMotif(chiffreOCR);
-        }
-        
-        return chiffre;
-    }
-    
-    public String determinerChiffreSelonMotif(String chiffre) {
-        return correspondanceMotifChiffre.get(chiffre);
-        
-    }
-    
-    public String decouperLeIemeChiffre(String entree, int i) {
-        int indexDebutChiffre = i * TAILLE_DU_CHIFFRE;
-        int indexFinChiffre = indexDebutChiffre + TAILLE_DU_CHIFFRE;
-        String ligneCaractere1 = entree.substring(indexDebutChiffre, indexFinChiffre);
-        String ligneCaractere2 = entree.substring(indexDebutChiffre + DEBUT_LIGNE_2, indexFinChiffre + DEBUT_LIGNE_2);
-        String ligneCaractere3 = entree.substring(indexDebutChiffre + DEBUT_LIGNE_3, indexFinChiffre + DEBUT_LIGNE_3);
-        
-        String chiffre = ligneCaractere1 + ligneCaractere2 + ligneCaractere3;
-        return chiffre;
-    }
-    
-    public void afficherLeChiffre(String ligneCaractere1, String ligneCaractere2, String ligneCaractere3) {
-        System.out.println(ligneCaractere1);
-        System.out.println(ligneCaractere2);
-        System.out.println(ligneCaractere3);
-    }
+
+	private static final int DEBUT_LIGNE_3 = 54;
+	private static final int DEBUT_LIGNE_2 = 27;
+	private static final int TAILLE_DU_CHIFFRE = 3;
+	private static final String MOTIF_CHIFFRE_ZERO = " _ " + "| |" + "|_|";
+	private static final String MOTIF_CHIFFRE_UN = "   " + "  |" + "  |";
+	private static final String MOTIF_CHIFFRE_DEUX = " _ " + " _|" + "|_ ";
+	private static final String MOTIF_CHIFFRE_TROIS = " _ " + " _|" + " _|";
+	private static final String MOTIF_CHIFFRE_QUATRE = "   " + "|_|" + "  |";
+	private static final String MOTIF_CHIFFRE_CINQ = " _ " + "|_ " + " _|";
+	private static final String MOTIF_CHIFFRE_SIX = " _ " + "|_ " + "|_|";
+	private static final String MOTIF_CHIFFRE_SEPT = " _ " + "  |" + "  |";
+	private static final String MOTIF_CHIFFRE_HUIT = " _ " + "|_|" + "|_|";
+	private static final String MOTIF_CHIFFRE_NEUF = " _ " + "|_|" + " _|";
+	private static Map<String, String> correspondanceMotifChiffre = new HashMap<String, String>();
+	static {
+		correspondanceMotifChiffre.put(MOTIF_CHIFFRE_ZERO, "0");
+		correspondanceMotifChiffre.put(MOTIF_CHIFFRE_UN, "1");
+		correspondanceMotifChiffre.put(MOTIF_CHIFFRE_DEUX, "2");
+		correspondanceMotifChiffre.put(MOTIF_CHIFFRE_TROIS, "3");
+		correspondanceMotifChiffre.put(MOTIF_CHIFFRE_QUATRE, "4");
+		correspondanceMotifChiffre.put(MOTIF_CHIFFRE_CINQ, "5");
+		correspondanceMotifChiffre.put(MOTIF_CHIFFRE_SIX, "6");
+		correspondanceMotifChiffre.put(MOTIF_CHIFFRE_SEPT, "7");
+		correspondanceMotifChiffre.put(MOTIF_CHIFFRE_HUIT, "8");
+		correspondanceMotifChiffre.put(MOTIF_CHIFFRE_NEUF, "9");
+	}
+
+	public String decoder(String entree) {
+		String chiffre = "";
+
+		for (int i = 0; i < 9; i++) {
+			String chiffreOCR = this.decouperLeIemeChiffre(entree, i);
+			chiffre += this.determinerChiffreSelonMotif(chiffreOCR);
+		}
+
+		return chiffre;
+	}
+
+	public String determinerChiffreSelonMotif(String chiffre) {
+		return correspondanceMotifChiffre.get(chiffre);
+
+	}
+
+	public String decouperLeIemeChiffre(String entree, int i) {
+		int indexDebutChiffre = i * TAILLE_DU_CHIFFRE;
+		int indexFinChiffre = indexDebutChiffre + TAILLE_DU_CHIFFRE;
+		String ligneCaractere1 = entree.substring(indexDebutChiffre, indexFinChiffre);
+		String ligneCaractere2 = entree.substring(indexDebutChiffre + DEBUT_LIGNE_2, indexFinChiffre + DEBUT_LIGNE_2);
+		String ligneCaractere3 = entree.substring(indexDebutChiffre + DEBUT_LIGNE_3, indexFinChiffre + DEBUT_LIGNE_3);
+
+		return ligneCaractere1 + ligneCaractere2 + ligneCaractere3;
+	}
+
+	public void afficherLeChiffre(String ligneCaractere1, String ligneCaractere2, String ligneCaractere3) {
+		System.out.println(ligneCaractere1);
+		System.out.println(ligneCaractere2);
+		System.out.println(ligneCaractere3);
+	}
+
+	public boolean estUnCompteValide(String entreedecodee) {
+		int checksum = 0;
+		for (int i = 0; i < entreedecodee.length(); i++) {
+			int chiffre = Character.getNumericValue(entreedecodee.charAt(i));
+			checksum += chiffre * (9 - i);
+		}
+		return (checksum % 11) == 0;
+	}
 }
--- bankocr/src/test/java/fr/jul/kata/bankocr/DecodeurOCRTest.java
+++ bankocr/src/test/java/fr/jul/kata/bankocr/DecodeurOCRTest.java
@@ -5,56 +5,81 @@ import static org.fest.assertions.Assertions.assertThat;
 import org.junit.Test;
 
 public class DecodeurOCRTest {
-    
-    @Test
-    public void peutReconnaitre000000000() {
-        DecodeurOCR decodeurOCR = new DecodeurOCR();
-        
-        String entreeDecodee = decodeurOCR.decoder(this.fabriquerUneEntreeDeZero());
-        
-        assertThat(entreeDecodee).isEqualTo("000000000");
-    }
-    
-    private String fabriquerUneEntreeDeZero() {
-        return " _  _  _  _  _  _  _  _  _ " + "| || || || || || || || || |" + "|_||_||_||_||_||_||_||_||_|" + "                           ";
-    }
-    
-    @Test
-    public void peutReconnaitre111111111() {
-        DecodeurOCR decodeurOCR = new DecodeurOCR();
-        
-        String entreedecodee = decodeurOCR.decoder(this.fabriquerUneEntreeDeUn());
-        
-        assertThat(entreedecodee).isEqualTo("111111111");
-    }
-    
-    private String fabriquerUneEntreeDeUn() {
-        return "                           " + "  |  |  |  |  |  |  |  |  |" + "  |  |  |  |  |  |  |  |  |" + "                           ";
-    }
-    
-    @Test
-    public void peutReconnaitre222222222() {
-        DecodeurOCR decodeurOCR = new DecodeurOCR();
-        
-        String entreeDecodee = decodeurOCR.decoder(this.fabriquerUneEntreeDeDeux());
-        
-        assertThat(entreeDecodee).isEqualTo("222222222");
-    }
-    
-    private String fabriquerUneEntreeDeDeux() {
-        return " _  _  _  _  _  _  _  _  _ " + " _| _| _| _| _| _| _| _| _|" + "|_ |_ |_ |_ |_ |_ |_ |_ |_ " + "                           ";
-    }
-    
-    @Test
-    public void peutReconnaitre123456789() {
-        DecodeurOCR decodeurOCR = new DecodeurOCR();
-        
-        String entreeDecodee = decodeurOCR.decoder(this.fabriquerUneEntree123456789());
-        
-        assertThat(entreeDecodee).isEqualTo("123456789");
-    }
-    
-    private String fabriquerUneEntree123456789() {
-        return "    _  _     _  _  _  _  _ " + "  | _| _||_||_ |_   ||_||_|" + "  ||_  _|  | _||_|  ||_| _|";
-    }
+
+	@Test
+	public void peutReconnaitre000000000() {
+		DecodeurOCR decodeurOCR = new DecodeurOCR();
+
+		String entreeDecodee = decodeurOCR.decoder(this.fabriquerUneEntreeDeZero());
+
+		assertThat(entreeDecodee).isEqualTo("000000000");
+	}
+
+	private String fabriquerUneEntreeDeZero() {
+		return " _  _  _  _  _  _  _  _  _ " + "| || || || || || || || || |" + "|_||_||_||_||_||_||_||_||_|" + "                           ";
+	}
+
+	@Test
+	public void peutReconnaitre111111111() {
+		DecodeurOCR decodeurOCR = new DecodeurOCR();
+
+		String entreedecodee = decodeurOCR.decoder(this.fabriquerUneEntreeDeUn());
+
+		assertThat(entreedecodee).isEqualTo("111111111");
+	}
+
+	private String fabriquerUneEntreeDeUn() {
+		return "                           " + "  |  |  |  |  |  |  |  |  |" + "  |  |  |  |  |  |  |  |  |" + "                           ";
+	}
+
+	@Test
+	public void peutReconnaitre222222222() {
+		DecodeurOCR decodeurOCR = new DecodeurOCR();
+
+		String entreeDecodee = decodeurOCR.decoder(this.fabriquerUneEntreeDeDeux());
+
+		assertThat(entreeDecodee).isEqualTo("222222222");
+	}
+
+	private String fabriquerUneEntreeDeDeux() {
+		return " _  _  _  _  _  _  _  _  _ " + " _| _| _| _| _| _| _| _| _|" + "|_ |_ |_ |_ |_ |_ |_ |_ |_ " + "                           ";
+	}
+
+	@Test
+	public void peutReconnaitre123456789() {
+		DecodeurOCR decodeurOCR = new DecodeurOCR();
+
+		String entreeDecodee = decodeurOCR.decoder(this.fabriquerUneEntree123456789());
+
+		assertThat(entreeDecodee).isEqualTo("123456789");
+	}
+
+	private String fabriquerUneEntree123456789() {
+		return "    _  _     _  _  _  _  _ " + "  | _| _||_||_ |_   ||_||_|" + "  ||_  _|  | _||_|  ||_| _|";
+	}
+
+	@Test
+	public void leNumeroDeCompte345882865EstValide() {
+		DecodeurOCR decodeur = new DecodeurOCR();
+		String entreedecodee = decodeur.decoder(fabriquerUnCompteValide());
+
+		boolean estUnCompteValide = decodeur.estUnCompteValide(entreedecodee);
+
+		assertThat(estUnCompteValide).isTrue();
+	}
+
+	private String fabriquerUnCompteValide() {
+		return " _     _  _  _  _  _  _  _ " + " _||_||_ |_||_| _||_||_ |_ " + " _|  | _||_||_||_ |_||_| _|";
+
+	}
+
+	@Test
+	public void leNumeroDeCompte222222222NEstPasValide() {
+		DecodeurOCR decodeur = new DecodeurOCR();
+		String entreedecodee = decodeur.decoder(fabriquerUneEntreeDeDeux());
+
+		boolean estUnCompteValide = decodeur.estUnCompteValide(entreedecodee);
+
+		assertThat(estUnCompteValide).isFalse();
+	}
 }
